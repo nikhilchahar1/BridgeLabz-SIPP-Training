b32f1b97f6fd40b625d56b4265a5be6ea523fd0c
Nikhil Chahar <nikhilchahar592@gmail.com>
2025-06-23 12:24:24 +0530

    Added solution for greatest factor using for loop

---
+++ Control_Flow/GreatestFactorFor.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GreatestFactorFor {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // User input for number
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        int greatestFactor = 1;
+        for (int i = number - 1; i >= 1; i--) {
+            if (number % i == 0) {
+                greatestFactor = i;
+                break;
+            }
+        }
+        System.out.println("The greatest factor of " + number + " besides itself is: " + greatestFactor);
+        scanner.close();
+    }
+}
