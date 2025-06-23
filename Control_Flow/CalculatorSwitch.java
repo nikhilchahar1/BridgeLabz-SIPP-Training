ec630dce7edc52ca2aaef10750fc866c58922d30
Nikhil Chahar <nikhilchahar592@gmail.com>
2025-06-23 11:48:17 +0530

    Added solution for calculator using switch

---
+++ Control_Flow/CalculatorSwitch.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class CalculatorSwitch {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // User input for numbers and operator
+        System.out.print("Enter the first number: ");
+        double first = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double second = scanner.nextDouble();
+        System.out.print("Enter operator (+, -, *, /): ");
+        String op = scanner.next();
+        double result;
+        switch (op) {
+            case "+":
+                result = first + second;
+                System.out.println("Result: " + result);
+                break;
+            case "-":
+                result = first - second;
+                System.out.println("Result: " + result);
+                break;
+            case "*":
+                result = first * second;
+                System.out.println("Result: " + result);
+                break;
+            case "/":
+                if (second != 0) {
+                    result = first / second;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid Operator.");
+        }
+        scanner.close();
+    }
+}
