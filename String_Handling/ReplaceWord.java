21feac7e3efc97de49dfbdf8186cfe11611cfdbe
Nikhil Chahar <nikhilchahar592@gmail.com>
2025-06-29 22:31:23 +0530

    Added solution for replacing word

--- String_Handling/ReplaceWord.java
+++ String_Handling/ReplaceWord.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class ReplaceWord {
     public static void main(String[] args) {
+
+        // Taking User Input
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         String wordToReplace = sc.next();
