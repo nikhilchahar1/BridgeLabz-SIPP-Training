81b3e70515289c3b4ee195ce320acae2fbbce12d
Nikhil Chahar <nikhilchahar592@gmail.com>
2025-09-17 11:17:43 +0530

    Added Main3 example demonstrating annotation usage

---
+++ Reflection_Annotation/Main3.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Main3 {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("Hello");
+        list.add(123);
+        System.out.println(list);
+    }
+}
